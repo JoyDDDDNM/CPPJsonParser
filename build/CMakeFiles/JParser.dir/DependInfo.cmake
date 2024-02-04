7af5ca3a7c6c2e35b808deaba9b8046e54f69c85
JoyDDDDNM <847714945@qq.com>
2024-02-04 12:02:24 +1100

    README.md added

---
+++ build/CMakeFiles/JParser.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/wuruidong/Desktop/Json-Parser/json-parser/JObject.cpp" "CMakeFiles/JParser.dir/JObject.cpp.o" "gcc" "CMakeFiles/JParser.dir/JObject.cpp.o.d"
+  "/Users/wuruidong/Desktop/Json-Parser/json-parser/Parser.cpp" "CMakeFiles/JParser.dir/Parser.cpp.o" "gcc" "CMakeFiles/JParser.dir/Parser.cpp.o.d"
+  "/Users/wuruidong/Desktop/Json-Parser/json-parser/main.cpp" "CMakeFiles/JParser.dir/main.cpp.o" "gcc" "CMakeFiles/JParser.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
